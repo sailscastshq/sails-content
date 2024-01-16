258c824d07135f81284f75f26e6ed7a89961cc3a
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-01-16 12:28:30 +0100

    feat(plugin-sails-content): update config to use an object to take layout as well

--- packages/plugin-sails-content/lib/generate-content.js
+++ packages/plugin-sails-content/lib/generate-content.js
@@ -3,22 +3,27 @@ const path = require('path')
 const render = require('./render')
 const writeHtmlToOutput = require('./write-html-to-output')
 
-async function generateContent(inputDir, outputDir = '.tmp/public') {
-  const files = await fs.readdir(inputDir)
+async function generateContent(config) {
+  config.outputDir = config.outputDir ? config.outputDir : '.tmp/public'
+  const files = await fs.readdir(config.inputDir)
 
   for (const file of files) {
-    const filePath = path.join(inputDir, file)
+    const filePath = path.join(config.inputDir, file)
     const fileStats = await fs.stat(filePath)
 
     if (fileStats.isDirectory()) {
-      await generateContent(filePath, path.join(outputDir, file))
+      await generateContent({
+        inputDir: filePath,
+        outputDir: path.join(config.outputDir, file)
+      })
     } else if (fileStats.isFile() && file.toLowerCase().endsWith('.md')) {
-      const { data, renderedHtml } = await render(filePath)
+      const { data, renderedHtml } = await render(filePath, config.layout)
       const outputFilePath = await writeHtmlToOutput(
         renderedHtml,
         file,
-        inputDir
+        config.inputDir
       )
+      // @ts-ignore
       sails.log.verbose(
         `[sails:content] Processed ${filePath} -> ${outputFilePath}`
       )
