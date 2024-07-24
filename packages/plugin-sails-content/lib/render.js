4cf7c4c9648810e52b3373ae379c1bdbcf8b162a
Kelvin Oghenerhoro Omereshone <kelvinomereshone@gmail.com>
2024-07-24 13:55:29 +0100

    feat(plugin-sails-content): update render to use sails.renderView

--- packages/plugin-sails-content/lib/render.js
+++ packages/plugin-sails-content/lib/render.js
@@ -1,9 +1,8 @@
 const fs = require('fs/promises')
 const matter = require('gray-matter')
 const showdown = require('showdown')
-const ejs = require('ejs')
 
-async function render(mdFile, layout) {
+async function render(sails, mdFile, layout) {
   const fileContent = await fs.readFile(mdFile, { encoding: 'utf8' })
   const { data, content } = matter(fileContent)
 
@@ -14,7 +13,12 @@ async function render(mdFile, layout) {
 
   const layoutContent = await fs.readFile(layout, { encoding: 'utf8' })
 
-  const renderedHtml = ejs.render(layoutContent, { data, content: htmlContent })
+  const renderedHtml = await sails.renderView(layoutContent, {
+    layout: false,
+    data,
+    content: htmlContent,
+    ...sails.config.views.locals
+  })
 
   return { data, renderedHtml }
 }
